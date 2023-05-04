897c23d47f620fc48bae58d1e71d724fdfee0079
georgeokez <georgeokez@gmail.com>
2023-05-04 01:59:37 +0100

    chore: server with metadata fetch features and mock user save feature

--- 02-managing-http-requests/02/server.js
+++ 02-managing-http-requests/02/server.js
@@ -1,8 +1,30 @@
-const http = require('http');
+const http = require("http");
+const services = require("../../services");
+const url = require("url");
+const jsonBody = require("body/json");
+
+const PORT = 8080;
 
 const server = http.createServer();
-server.on('request', (request, response) => {
-  
+
+server.on("request", (request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+
+  if (request.method === "GET" && parsedUrl.pathname === "/metadata") {
+    const { id } = parsedUrl.query;
+    const metadata = services.fetchImageMetadata(id);
+    console.log("Request headers: ", request.headers);
+  }
+
+  jsonBody(request, response, (err, body) => {
+    if (err) {
+      console.error(err);
+    }
+
+    services.createUser(body["userName"]);
+  });
 });
 
-server.listen(8080);
+server.listen(PORT, () => {
+  console.log("Server listening on port: ", PORT);
+});
