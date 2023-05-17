be0a9831d01f860e01dd0f7ea100e577d8cc4b1e
georgeokez <georgeokez@gmail.com>
2023-05-17 23:17:44 +0100

    feat: building a node server

--- 02-managing-http-requests/02/server.js
+++ 02-managing-http-requests/02/server.js
@@ -2,8 +2,17 @@ const http = require("http");
 const services = require("../../services");
 const url = require("url");
 const jsonBody = require("body/json");
+const fs = require("fs");
+const formidable = require("formidable");
 
 const PORT = 8080;
+
+/*
+const server = https.createServer({
+  key: fs.readFileSync("./key.pem"),
+  cert: fs.readFileSync("./cert.pem"),
+});
+*/
 
 const server = http.createServer();
 
@@ -13,16 +22,41 @@ server.on("request", (request, response) => {
   if (request.method === "GET" && parsedUrl.pathname === "/metadata") {
     const { id } = parsedUrl.query;
     const metadata = services.fetchImageMetadata(id);
-    console.log("Request headers: ", request.headers);
+    response.setHeader("Content-Type", "application/json");
+    response.statusCode = 200;
+    const serialisedJSON = JSON.stringify(metadata);
+    response.write(serialisedJSON);
+    response.end();
+
+    //console.log("Request headers: ", request.headers);
+  } else if (request.method === "POST" && parsedUrl.pathname === "/users") {
+    jsonBody(request, response, (err, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        services.createUser(body["userName"]);
+      }
+    });
+  } else if (request.method === "POST" && parsedUrl.pathname === "/upload") {
+    const form = new formidable.IncomingForm({
+      uploadDir: __dirname,
+      keepExtensions: true,
+      multiples: true,
+      maxFileSize: 5 * 1024 * 1024,
+    });
+
+    form
+      .parse(request)
+      .on("fileBegin", (name, file) => {
+        console.log("Our upload has started!");
+      })
+      .on("file", (name, value) => {
+        console.log("Field received: ");
+        console.log(name, value);
+      });
+  } else {
+    fs.createReadStream("../../index.html").pipe(response);
   }
-
-  jsonBody(request, response, (err, body) => {
-    if (err) {
-      console.error(err);
-    }
-
-    services.createUser(body["userName"]);
-  });
 });
 
 server.listen(PORT, () => {
--- 05-making-external-api-requests/01/request.js
+++ 05-making-external-api-requests/01/request.js
@@ -1,18 +1,15 @@
-const http = require('http');
+const http = require("http");
 
-const request = http.get(
-  'http://www.google.com',
-  (response) => {
-    console.log(`statusCode: ${response.statusCode}`);
-    console.log(response.headers);
+const request = http.get("http://www.google.com", (response) => {
+  console.log(`statusCode: ${response.statusCode}`);
+  console.log(response.headers);
 
-    response.on('data', (chunk) => {
-      console.log('This is a chunk: \n');
-      console.log(chunk.toString());
-    });
-  }
-);
+  response.on("data", (chunk) => {
+    console.log("This is a chunk: \n");
+    console.log(chunk.toString());
+  });
+});
 
-request.on('error', (err) => {
+request.on("error", (err) => {
   console.error(err);
 });
